2cc0481e584404c5d636ca2deddebff6d26f2c9e
Daniel Massey <masseyd1396@gmail.com>
2022-01-31 17:37:31 +0000

    Publish MassOS website

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+  The MassOS Website - Copyright (C) 2022 MassOS Developers.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+let subtitles = [
+  "Minimal, without compromising on features.",
+  "Completely independent, built from scratch.",
+  "A modern, yet lightweight desktop experience.",
+]
+
+function indexload() {
+  let subtitle = subtitles[parseInt(Math.random(subtitles.length) * 3)]
+  document.getElementById("subtitle").innerText = subtitle
+}
